f9f135dd47887f68d13379d826173fe35ccec85b
Semir Iljazi <semiriljazi@MacBook-Pro.home>
2020-10-15 16:20:18 +0200

    Rectangle

---
+++ rectangle/rectangle.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const c1 = fs.readFileSync(process.argv[2],"utf8").split("\n")
+const c2 = fs.readFileSync(process.argv[3],"utf8").split("\n")
+
+function rectangle(tab1, tab2){
+    for (var i = 0; i < tab2.length; i++){  // i = Ligne
+       for (var j = 0; j < tab2[i].length; j++){  // j = Colone
+
+         if (tab2[i][j] == tab1[0][0] && 
+            tab2[i][j+1] == tab1[0][1] &&
+             tab2[i][j+2] == tab1[0][2]){
+
+               if (tab2[i+1][j] == tab1[1][0] &&
+                 tab2[i+1][j+1] == tab1[1][1] &&
+                  tab2[i+1][j+2] == tab1[1][2]){
+
+                   if (tab2[i+2][j] == tab1[2][0] &&
+                     tab2[i+2][j+1] == tab1[2][1] &&
+                      tab2[i+2][j+2] == tab1[2][2]){
+
+                      console.log("Ligne " + i);
+                      console.log("Colone " + j);
+
+                    }
+                }
+            }      
+        }
+    }
+}
+  
+rectangle(c1, c2);
